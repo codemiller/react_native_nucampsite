3423a492ef97ec29b3a5ef78da33fdbda5988fb6
codemiller <ericzacharymiller@gmail.com>
2022-09-07 13:41:39 -0400

    workshop week 2

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,8 +1,155 @@
+import { useState } from 'react'
+import { FlatList, StyleSheet, Text, View, Button, Modal } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
+import { Rating, Input } from 'react-native-elements'
+import { postComment } from '../features/comments/commentsSlice';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-    return <RenderCampsite campsite={campsite} />;
+    const comments = useSelector((state) => state.comments);
+    const favorites = useSelector((state) => state.favorites);
+    const dispatch = useDispatch();
+
+    const [showModal, setShowModal] = useState(false);
+    const [rating, setRating] = useState(5);
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
+
+    const handleSubmit = () => {
+        const newComment = {
+            author,
+            rating,
+            text,
+            campsiteId: campsite.id
+        };
+        dispatch(postComment(newComment));
+        setShowModal(!showModal);
+    }
+
+    const resetForm = () => {
+        setRating(5);
+        setAuthor('');
+        setText('');
+    }
+
+    const renderCommentItem = ({ item }) => {
+        return (
+            <View style={styles.commentItem}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Rating
+                    style={{ fontSize: 12, paddingVertical: 5, alignItems: 'flex-start' }}
+                    startingValue={item.rating}
+                    imageSize={10}
+                    readonly
+                >{item.rating} Stars
+                </Rating>
+                <Text style={{ fontSize: 12 }}>
+                    {`-- ${item.author}, ${item.date}`}
+                </Text>
+            </View>
+        );
+    };
+
+    return (
+        <>
+            <FlatList
+                data={comments.commentsArray.filter(
+                    (comment) => comment.campsiteId === campsite.id
+                )}
+                renderItem={renderCommentItem}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={{
+                    marginHorizontal: 20,
+                    paddingVertical: 20
+                }}
+                ListHeaderComponent={
+                    <>
+                        <RenderCampsite
+                            campsite={campsite}
+                            isFavorite={favorites.includes(campsite.id)}
+                            markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                            onShowModal={() => setShowModal(!showModal)}
+                        />
+                        <Text style={styles.commentsTitle}>Comments</Text>
+                    </>
+                }
+            />
+            <Modal
+                animationType='slide'
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+                <View style={styles.modal}>
+                    <Rating
+                        showRating
+                        startingValue={rating}
+                        imageSize={40}
+                        onFinishRating={(rating) => setRating(rating)}
+                        style={{ paddingVertical: 10 }}
+                    />
+                    <Input
+                        placeholder='Author'
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        leftIconContainerStyle={{ paddingRight: 10 }}
+                        onChangeText={(author) => setAuthor(author)}
+                        value={text}
+                    />
+                    <Input
+                        placeholder='Comment'
+                        leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+                        leftIconContainerStyle={{ paddingRight: 10 }}
+                        onChangeText={(text) => setText(text)}
+                        value={text}
+                    />
+                    <View style={{ margin: 10 }}>
+                        <Button
+                            onPress={() => {
+                                handleSubmit();
+                                resetForm();
+                            }}
+                            title='Submit'
+                            color='#5637DD'
+
+                        />
+                    </View>
+                    <View style={{ margin: 10 }}>
+                        <Button
+                            onPress={() => {
+                                setShowModal(!showModal);
+                                resetForm();
+                            }}
+                            color='#808080'
+                            title='Cancel'
+                        />
+                    </View>
+                </View>
+            </Modal>
+        </>
+    );
 };
+
+const styles = StyleSheet.create({
+    commentsTitle: {
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#43484D',
+        padding: 10,
+        paddingTop: 30
+    },
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#fff'
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    }
+});
 
 export default CampsiteInfoScreen;
--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,19 +1,20 @@
-import { ScrollView } from "react-native-web"
-import { Card } from "react-native-elements"
-import { Text } from "react-native"
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 
 const ContactScreen = () => {
-    return <ScrollView>
-        <Card wrapperStyle={{margin: 20}}>
-            <Card.Title> Contact Information</Card.Title>
-            <Text> 1 Nucamp Way </Text>
-            <Text> Seattle, WA 98001</Text>
-            <Text style={{marginBottom: 10}}> U.S.A.</Text>
-            <Text> Phone: 1-206-555-1234</Text>
-            <Text> Email: campsites@nucamp.co</Text>
-            <Card.Divider />
-        </Card>
-    </ScrollView>
+    return (
+        <ScrollView>
+            <Card wrapperStyle={{ margin: 20 }}>
+                <Card.Title> Contact Information</Card.Title>
+                <Card.Divider />
+                <Text> 1 Nucamp Way </Text>
+                <Text> Seattle, WA 98001</Text>
+                <Text style={{ marginBottom: 10 }}> U.S.A.</Text>
+                <Text> Phone: 1-206-555-1234</Text>
+                <Text> Email: campsites@nucamp.co</Text>
+            </Card>
+        </ScrollView>
+    )
 }
 
 
